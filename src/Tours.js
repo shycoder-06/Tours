fa9c5c4b724cdc903b36e6dbc470dc8aa9f7ff61
Shyla <shailajasrinivas91@gmail.com>
2022-12-24 21:52:40 -0500

    tours component - updated to map tour props

--- src/Tours.js
+++ src/Tours.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import Tour from './Tour'
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-const Tours = () => {
-  return <h2>tours component</h2>
-}
-
-export default Tours
+export default Tours;
