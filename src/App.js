8ddc556e49bf4234f35285b1b246eea63bf0a717
Shyla <shailajasrinivas91@gmail.com>
2022-12-24 21:50:48 -0500

    tour component updated with props and function removeTour updated in app.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [ loading, setLoading ] = useState(true);
   const [ tours, setTours ] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
   const fetchTours = async () => {
     setLoading(true);
+    try{
     const response = await fetch(url);
     const tours = await response.json();
+    setLoading(false);
+    setTours(tours);
+    }
+    catch(error){
+      setLoading(false);
+      console.log(error);
+    }
     console.log(tours);
 
   }
@@ -24,8 +37,20 @@ fetchTours();
       </main>
     )
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
   return <main> 
-    <Tours/>
+    <Tours tours={tours} removeTour={removeTour}/>
   </main>
 }
 export default App
