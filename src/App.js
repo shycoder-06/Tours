9cbfdfa804e3c38f6326265ab71d20ac4b4e6414
Shyla <shailajasrinivas91@gmail.com>
2022-12-24 21:40:39 -0500

    UseState to set tours and useeffect to fetchtours

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
-}
+  const [ loading, setLoading ] = useState(true);
+  const [ tours, setTours ] = useState([]);
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
 
+  }
+  useEffect(() => {
+fetchTours();
+  }, [])
+  if(loading){
+    return(
+      <main>
+      <Loading />
+      </main>
+    )
+  }
+  return <main> 
+    <Tours/>
+  </main>
+}
 export default App
